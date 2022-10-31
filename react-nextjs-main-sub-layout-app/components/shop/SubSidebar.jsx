e27276f336717910ee078f4d430d124d53820763
Higashi-Kota <higashi.kota@gri.jp>
2022-10-31 10:03:53 +0900

    add accordion

--- react-nextjs-main-sub-layout-app/components/shop/SubSidebar.jsx
+++ react-nextjs-main-sub-layout-app/components/shop/SubSidebar.jsx
@@ -1,6 +1,73 @@
 import {css, cx} from '@emotion/css';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
+
+import {MdLocalCafe} from 'react-icons/md';
+import {GrCafeteria, GrRestaurant} from 'react-icons/gr';
+import {GiSadCrab, GiSushis} from 'react-icons/gi';
 
 const SubSidebar = () => {
+  const items = [
+    {
+      heading: `レストラン`,
+      icon: ({className = css``}) => {
+        return <GrCafeteria size={24} className={className} />;
+      },
+      subItems: [
+        {
+          heading: `なばの`,
+        },
+        {
+          heading: `フェイバレンタイン`,
+        },
+        {
+          heading: `スピーゲル`,
+        },
+      ],
+    },
+    {
+      heading: `寿司`,
+      icon: ({className = css``}) => {
+        return <GiSushis size={24} className={className} />;
+      },
+      subItems: [
+        {
+          heading: `かっぱ寿司`,
+        },
+      ],
+    },
+    {
+      heading: `カフェ`,
+      icon: ({className = css``}) => {
+        return <MdLocalCafe size={24} className={className} />;
+      },
+      subItems: [
+        {
+          heading: `スタバ`,
+        },
+        {
+          heading: `ドトール`,
+        },
+        {
+          heading: `タリーズ`,
+        },
+      ],
+    },
+    {
+      heading: `カニ`,
+      icon: ({className = css``}) => {
+        return <GiSadCrab size={24} className={className} />;
+      },
+      subItems: [],
+    },
+  ];
+
   return (
     <div
       className={cx(
@@ -17,9 +84,64 @@ const SubSidebar = () => {
         `
       )}
     >
-      <h2>Shop</h2>
-      <h2>Sub</h2>
-      <h2>Sidebar</h2>
+      <Accordion allowZeroExpanded className={`select-none`}>
+        {items.map((item, i) => {
+          return (
+            <AccordionItem key={i}>
+              <AccordionItemHeading>
+                <AccordionItemButton
+                  className={cx(
+                    `accordion__button`,
+                    `hover:!bg-gray-100 hover:!cursor-pointer`,
+                    `!bg-transparent !text-inherit`,
+                    `!border-b-2 !border-r-2`,
+                    `flex items-center w-full`,
+                    `relative`,
+                    `!pl-10`,
+                    css`
+                      &::before {
+                        opacity: ${item.subItems.length === 0 ? 0 : 1};
+                        visibility: ${item.subItems.length === 0
+                          ? `hidden`
+                          : `visble`};
+                        display: inline-block;
+                        content: '';
+                        position: absolute;
+                        right: 20px;
+                      }
+                    `
+                  )}
+                >
+                  {item.heading}
+                  {item.icon({className: `absolute left-2`})}
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              {item.subItems.map((subItem, j) => {
+                return (
+                  <AccordionItemPanel
+                    key={i + j}
+                    className={cx(
+                      `accordion__panel`,
+                      `hover:!bg-gray-100 hover:!cursor-pointer`,
+                      `!bg-transparent !text-inherit`,
+                      `!border-t-2`,
+                      `flex items-center w-full`,
+                      `relative`,
+                      `!pl-10`,
+                      css`
+                        animation: fadein 0.35s ease-in;
+                        animation-name: none;
+                      `
+                    )}
+                  >
+                    {subItem.heading}
+                  </AccordionItemPanel>
+                );
+              })}
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
     </div>
   );
 };
